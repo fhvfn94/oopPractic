ab44923f9b6fea2b34c98afbfb585e886220bcee
Eldar <fhvfn94@gmail.com>
2022-11-20 23:49:53 +0300

    add 2 task

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Person maxim = new Person("Максим", 1988, "Минск");
-        Person anya = new Person("Аня", 1993, "Москва");
-        Person katya = new Person("Катя", 1992, "Калининград");
-        Person artem = new Person("Артем", 1995, "Москва");
+        Person maxim = new Person("Максим", 1988, "Минск", "бренд-менеджером");
+        Person anya = new Person("Аня", 1993, "Москва", "методистом образовательных программ");
+        Person katya = new Person("Катя", 1992, "Калининград", "продакт-менеджером");
+        Person artem = new Person("Артем", 1995, "Москва", "продакт-менеджером");
         maxim.answer();
         anya.answer();
         katya.answer();
--- src/Person.java
+++ src/Person.java
@@ -2,14 +2,17 @@ public class Person {
     String name;
     int yearOfBirth;
     String town;
+    //второе задание
+    String jobTitle;
 
-    Person(String name, int yearOfBirth, String town) {
+    Person(String name, int yearOfBirth, String town, String jobTitle) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
         this.town = town;
+        this.jobTitle = jobTitle;
     }
 
     void answer() {
-        System.out.println("Привет! Меня зовут " + name + " Я из города " + town +  " Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + " Я из города " + town +  " Я родился в " + yearOfBirth + " году. Я работаю " + jobTitle + " Будем знакомы!");
     }
 }
