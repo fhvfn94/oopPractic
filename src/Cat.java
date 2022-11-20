f1d910963dfb2b6d22eae543f83d3dee265c6f51
Eldar <fhvfn94@gmail.com>
2022-11-20 23:16:14 +0300

    первÐыое прÐактиÑвторое ¸Ðео опо ООÐпо ООПÐ

--- src/Cat.java
+++ src/Cat.java
@@ -2,6 +2,15 @@ public class Cat {
     String name;
     int age;
 
+    Cat(String name) {
+        this.name = name;
+        age = 1;
+    }
+    Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     void meow() {
         System.out.println("meow");
         System.out.println("Меня зовут " + name);
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Cat a = new Cat();
-        a.age = 2;
-        a.name = "Anton";
+        Cat a = new Cat("Anton", 2);
+        //a.age = 2;
+        //a.name = "Anton";
         a.meow();
 
-        Cat b = new Cat();
-        b.name = "Andrey";
-        b.age = 45;
+        Cat b = new Cat("Andrey");
+       // b.name = "Andrey";
+        //b.age = 45;
         b.meow();
     }
 }
