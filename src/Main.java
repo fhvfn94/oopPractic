7eb5df3a19c98c2003688b4a53e19ffbeca33b1e
Eldar <fhvfn94@gmail.com>
2022-11-23 23:38:46 +0300

    add 1 task

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Person maxim = new Person("Максим", 1988, "Минск", "бренд-менеджером");
+        Person maxim = new Person("", 1988, "Минск", "бренд-менеджером");
         Person anya = new Person("Аня", 1993, "Москва", "методистом образовательных программ");
         Person katya = new Person("Катя", 1992, "Калининград", "продакт-менеджером");
         Person artem = new Person("Артем", 1995, "Москва", "продакт-менеджером");
+        maxim.setName(null);
         maxim.answer();
         anya.answer();
         katya.answer();
@@ -19,5 +20,8 @@ public class Main {
         System.out.println(bmw.toString());
         System.out.println(kia.toString());
         System.out.println(hyundai.toString());
+
+        Flower rose = new Flower("", "Голандии", 35.59, 1);
+        System.out.println(rose.toStringRose());
     }
 }
