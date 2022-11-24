30a1777a2cf99336377115340dfcdff1dcf27ae2
Eldar <fhvfn94@gmail.com>
2022-11-24 21:01:14 +0300

    add get&set

--- src/Cat.java
+++ src/Cat.java
@@ -1,19 +1,35 @@
+import java.time.LocalDate;
+
 public class Cat {
     String name;
-    int age;
+    int yearBirth;
 
     Cat(String name) {
-        this.name = name;
-        age = 1;
+        this(name, 0);
     }
     Cat(String name, int age) {
         this.name = name;
-        this.age = age;
+        if (age >= 0) {
+            this.yearBirth = LocalDate.now().getYear() - age;
+        } else {
+            this.yearBirth = LocalDate.now().getYear() - Math.abs(age);
+        }
     }
 
     void meow() {
         System.out.println("meow");
-        System.out.println("Меня зовут " + name);
-        System.out.println("возраст " + age);
+        System.out.println("Меня зовут " + getName());
+        System.out.println("возраст " + getAge());
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return LocalDate.now().getYear() - yearBirth;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Cat a = new Cat("Anton", 2);
-        //a.age = 2;
-        //a.name = "Anton";
         a.meow();
 
-        Cat b = new Cat("Andrey");
-       // b.name = "Andrey";
-        //b.age = 45;
+
+        Cat b = new Cat("Andrey", 12);
         b.meow();
     }
 }
