87dd60ae81fc4e1cdb563e65b641df59acd60c7d
Эльдар К. Ишмуханбетов <IshmukhanbetovEK@mcms.local>
2023-01-10 20:12:33 +0300

    add 2 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import transport.Car;
 import transport.Car.Key;
+import transport.train;
+import transport.transport;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,5 +19,13 @@ public class Main {
 
         Flower rose = new Flower("", "Голандии", 35.59, 1);
         System.out.println(rose.toStringRose());
+
+        train lasochka = new train("Ласточка", "B-901", 2011, "России", "green", 301, 3500, 10, "Белорусского вокзала", "Минск-Пассажирский", 11);
+        System.out.println("Поезд, " + lasochka.getBrand() + " модель " + lasochka.getModel() + " , " + lasochka.getYear() + " год выпуска в " + lasochka.getCountry() + ", скорость передвижения — " + lasochka.getMaxSpeed() + " км/ч, отходит от " + lasochka.getStartPoint() + " и следует до станции " + lasochka.getFinishPoint() + ". Цена поездки — " + lasochka.getCostWay() + " рублей, в поезде " + lasochka.getRailwayCarriage() + " вагонов.");
+
+        train leningrad = new train("Ленинград", "D-125", 2019, "России", "green", 270, 1700, 10, "Ленинградского", "Ленинград-Пассажирский", 8);
+        System.out.println("Поезд, " + leningrad.getBrand() + " модель " + leningrad.getModel() + " , " + leningrad.getYear() + " год выпуска в " + lasochka.getCountry() + ", скорость передвижения — " + leningrad.getMaxSpeed() + " км/ч, отходит от " + leningrad.getStartPoint() + " вокзала и следует до станции " + leningrad.getFinishPoint() + ". Цена поездки — " + leningrad.getCostWay() + " рублей, в поезде " + leningrad.getRailwayCarriage() + " вагонов.");
+
+
     }
 }
